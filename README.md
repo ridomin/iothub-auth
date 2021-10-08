3b906782aa561917ce89b8ed9da1464922c37fde
rido-min <rido-min@users.noreply.github.com>
2021-10-08 10:58:28 -0700

    first cut

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # iothub-auth
-Generates Shared Access Keys to connect to Azure IoT Hub
+
+Generates Shared Access Keys to connect to Azure IoT Hub from ES6 modules
+
+## Usage
+
+```html
+<script type="module">
+import {getIoTHubV2Credentials} from '../iothub-auth.js'
+; (async ()=> {
+    const [username, password] = await getIoTHubV2Credentials(
+        'host', 
+        'device', 
+        'MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA=')
+    document.write(username,password)
+})()
+</script>
+```
+
+## apiversion
+
+this preview uses an experimental IoTHub api-version available in private preview
+
+
+
+
---
+++ iothub-auth.js
@@ -0,0 +1,29 @@
+/**
+ * Creates the creadentials (username/password) to connect to AzureIoT Hub using the preview api-version
+ * 
+ * @param {string} hostname 
+ * @param {string} deviceId 
+ * @param {string} key 
+ * @param {number} expiresInMins 
+ * @returns 
+ */
+export const getIoTHubV2Credentials = async (hostname, deviceId, key, expiresInMins) => {
+    const apiversion = '2021-06-30-preview'
+    const generateToken = async (resource, key, expires) => {
+        const createHmac = async (msg, key) => {
+            const keyBytes = Uint8Array.from(window.atob(key), c => c.charCodeAt(0))
+            const msgBytes = Uint8Array.from(msg, c => c.charCodeAt(0))
+            const cryptoKey = await window.crypto.subtle.importKey(
+                'raw', keyBytes, { name: 'HMAC', hash: 'SHA-256' },
+                true, ['sign']
+            )
+            const signature = await window.crypto.subtle.sign('HMAC', cryptoKey, msgBytes)
+            return window.btoa(String.fromCharCode(...new Uint8Array(signature)))
+        }
+        return await createHmac(`${resource}\n\n\n${expires}\n`, key)
+    }
+    const expires = Math.ceil(Date.now() + expiresInMins * 60)
+    const username = `av=${apiversion}&h=${hostname}&did=${deviceId}&am=SASb64&se=${expires}`
+    const password = await generateToken(`${hostname}\n${deviceId}`, key, expires)
+    return [username, password]
+}
