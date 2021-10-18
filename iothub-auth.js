a5b6119ba4fbeeaec51415233cbc32731702f0cf
ridomin <ridomin@live.com>
2021-10-18 01:10:21 -0700

    addv1 token

--- iothub-auth.js
+++ iothub-auth.js
@@ -1,5 +1,23 @@
 /**
- * Creates the credentials (username/password) to connect to AzureIoT Hub with SasKeys using the preview api-version
+ *
+ * @param {String} key
+ * @param {String} msg
+ * @returns {Promise<string>}
+ */
+ const createHmac = async (key, msg) => {
+    const keyBytes = Uint8Array.from(window.atob(key), c => c.charCodeAt(0))
+    const msgBytes = Uint8Array.from(msg, c => c.charCodeAt(0))
+    const cryptoKey = await window.crypto.subtle.importKey(
+      'raw', keyBytes, { name: 'HMAC', hash: 'SHA-256' },
+      true, ['sign']
+    )
+    const signature = await window.crypto.subtle.sign('HMAC', cryptoKey, msgBytes)
+    return window.btoa(String.fromCharCode(...new Uint8Array(signature)))
+}
+
+/**
+ * Creates the credentials (username/password) to connect to AzureIoT Hub with SasKeys 
+ * using the preview api-version 
  * 
  * @param {string} hostname 
  * @param {string} deviceId 
@@ -9,21 +27,38 @@
  */
 export const getIoTHubV2Credentials = async (hostname, deviceId, key, expiresInMins = 5) => {
     const apiversion = '2021-06-30-preview'
-    const generateToken = async (resource, key, expires) => {
-        const createHmac = async (msg, key) => {
-            const keyBytes = Uint8Array.from(window.atob(key), c => c.charCodeAt(0))
-            const msgBytes = Uint8Array.from(msg, c => c.charCodeAt(0))
-            const cryptoKey = await window.crypto.subtle.importKey(
-                'raw', keyBytes, { name: 'HMAC', hash: 'SHA-256' },
-                true, ['sign']
-            )
-            const signature = await window.crypto.subtle.sign('HMAC', cryptoKey, msgBytes)
-            return window.btoa(String.fromCharCode(...new Uint8Array(signature)))
-        }
-        return await createHmac(`${resource}\n\n\n${expires}\n`, key)
-    }
+
+    const generateToken = async (resource, key, expires) => 
+        await createHmac(`${resource}\n\n\n${expires}\n`, key)
+    
     const expires = Math.ceil(Date.now() + expiresInMins * 60)
     const username = `av=${apiversion}&h=${hostname}&did=${deviceId}&am=SASb64&se=${expires}`
     const password = await generateToken(`${hostname}\n${deviceId}`, key, expires)
+    return [username, password]
+}
+
+/**
+ * Creates the credentials (username/password) to connect to AzureIoT Hub with SasKeys 
+ * using the v1 api-version 
+ * 
+ * @param {string} hostname 
+ * @param {string} deviceId 
+ * @param {string} key 
+ * @param {number} expiresInMins 
+ * @returns 
+ */
+ export const getIoTHubV1Credentials = async (hostname, deviceId, key, expiresInMins = 5) => {
+    const apiversion = '2021-06-30-preview'
+    const generateToken = async (resourceUri, key, expiresv1) => {
+        resourceUri = encodeURIComponent(resourceUri)
+        const toSign = resourceUri + '\n' + expiresv1
+        const hmac = await createHmac(key, toSign)
+        const base64UriEncoded = encodeURIComponent(hmac)
+        let token = 'SharedAccessSignature sr=' + resourceUri + '&sig=' + base64UriEncoded + '&se=' + expiresv1
+        return token
+    }
+    const expires = Math.ceil(Date.now() + expiresInMins * 60)
+    const username = `${hostname}/${deviceId}/?api-version=2020-05-31-preview`
+    const password = await generateToken(`${hostname}/devices/${deviceId}`, key, expires)
     return [username, password]
 }
